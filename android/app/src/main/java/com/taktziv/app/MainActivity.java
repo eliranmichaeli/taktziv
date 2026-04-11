9af0cd9553517ca44bf629c2617331d87b70cf32
eliranmichaeli <eliran1456@gmail.com>
2026-04-11 15:02:49 +0300

    change

---
+++ android/app/src/main/java/com/taktziv/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.taktziv.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.taktziv.app',
+  appName: 'Taktziv',
+  webDir: 'dist'
+};
+
+export default config;
